155851441333b166f1b4090a82b2848ab79e3ec0
Anushka Sharma <anushka4404@gmail.com>
2026-01-05 00:59:43 +0530

    Fix CORS and cookie auth for Render

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -11,9 +11,9 @@ export const protectRoute = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
-    }
+    // if (!decoded) {
+    //   return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    // }
 
     const user = await User.findById(decoded.userId).select("-password");
 
@@ -26,6 +26,6 @@ export const protectRoute = async (req, res, next) => {
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(401).json({ message: "Internal server error" });
   }
 };
